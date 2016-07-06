9c68cdbc497cd4a0416ca00017deaead16ab0175
Mikhael <mikhael_sk@mail.ru>
2016-07-07 01:59:35 +0300

    in work with model

--- bin/models/MainModel.py
+++ bin/models/MainModel.py
@@ -1,15 +1,35 @@
 from .Library import *
 from ..Controller import *
+import os.path
+
 class MainModel():
     """files and folders model"""
     def __init__( self, myController ):
         self.controller = myController
         libName = "MyLibrary"
         self.library = Library( libName )
-        self.controller.logger.WriteToLog( "Library initialized: " + libName )
+        self.controller.PrintToLog( "Library initialized: " + libName )
         
-    def AddFolder( self, path ):
-        self.controller.logger.WriteToLog( "New Folder added: " + path )
+    def AddFolder( self, folderPath ):
+        #self.controller.logger.WriteToLog( "New Folder added: " + path )
+        #currrentTree = os.walk( folderPath )
+        for ( paths,dirs,files ) in os.walk( folderPath ):
+            for file in files:
+                self.controller.PrintToLog( "Found File: " + paths +"\\"+file)
+                self.AddFile( paths, file )
+
+    def AddFile( self, path, name ):
+        self._AddFileIntro(path, name)
+        self.controller.UpdateLibraryView()
+        #TODO
+
+    def SaveLibrary(self):
+        pass
+        #TODO
+            
+    def _AddFileIntro(self, path, name ):
+        self.library.AddNode(path, name)
+        
         
 
 
