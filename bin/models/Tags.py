532e8152f810d7fd91c3e052a9857d0b5e2ec3d3
Mikhael <mikhael_sk@mail.ru>
2016-07-08 17:02:59 +0300

    Saving Tag Library done

--- bin/models/Tags.py
+++ bin/models/Tags.py
@@ -1,14 +1,21 @@
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtCore import * 
 from PyQt5.QtXml import * 
 import os.path
 import sys
 
 class Tags():
-    """description of class"""
+    """class for working with tags model:
+        - adding tags
+        - deletening tags
+        - loading tags DB
+        - saving tags DB
+        """
     
     def __init__( self, myController ):
         self.controller = myController
+        self.wereAnyChanges = False
         self.tagList = []
         self.tagModel = QStandardItemModel( 0,1 )
         self.tagModel.setHeaderData( 0, Qt.Horizontal, QVariant( "TagList for Library:" ) )
@@ -16,17 +23,19 @@ class Tags():
         if os.path.exists( "myTags.xml" ) == False:
             self.controller.PrintToLog( "No Tag library found, created new one" )
         else:
-            self.parseXML()
+            self.LoadTagsFromXML()
     
     def InitDefaultTags( self ):
-        self.AddTag( "IMPORTANT" )
+        pass
+        #self.AddTag( "IMPORTANT" )
 
     def AddTag( self, name ):
         self.tagList.append( name )
         self.tagModel.appendRow( QStandardItem( name ) )
-        self.tagModel.sort(Qt.AscendingOrder)
+        self.tagModel.sort( Qt.AscendingOrder )
+        self.wereAnyChanges = True
 
-    def parseXML( self ):
+    def LoadTagsFromXML( self ):
         xmlParser = QXmlSimpleReader()
         xmlContentHandler = myXmlContentHandler( self )
         xmlFile = QFile( "myTags.xml" )
@@ -34,10 +43,39 @@ class Tags():
         xmlParser.setContentHandler( xmlContentHandler )  
       
         if( xmlParser.parse( xmlInputSource ) ):
-            self.controller.PrintToLog( "Parsed Tag-library Successfully!" )
+            self.controller.PrintToLog( "Tag-library successfully loaded!" )
         else:  
             self.controller.PrintToLog( "Parsing of Tag-library Failed!" )
 
+    def SaveTagModel( self ):
+        #if self.wereAnyChanges == False:
+        #    return
+        QFile.remove( "myTags_bcp.xml" )
+        xmlOldFile = QFile( "myTags.xml" )        
+        xmlOldFile.rename( "myTags_bcp.xml" )
+
+        xmlWriter = QXmlStreamWriter()
+        xmlFile = QFile( "myTags_new.xml" )
+        
+        if ( xmlFile.open( QIODevice.WriteOnly ) == False ):    
+            QMessageBox.warning( 0, "Error!", "Error opening file" )    
+        else :    
+            xmlWriter.setDevice( xmlFile )	
+            xmlWriter.writeStartDocument()
+            xmlWriter.writeStartElement( "TagLibrary" )
+            
+            for tagItem in self.tagList:
+                xmlWriter.writeStartElement( tagItem )
+                xmlWriter.writeEndElement() 
+                xmlWriter.autoFormattingIndent()
+            xmlWriter.writeEndElement()
+            xmlWriter.writeEndDocument()
+        self.controller.PrintToLog( "TagLibrary successfully saved!" )
+        #if everything was written successfully, replace a tag file with new one
+        QFile.remove( "myTags.xml" )
+        xmlFile.rename( "myTags.xml" )
+
+        
 class myXmlContentHandler( QXmlDefaultHandler ):    
     def __init__( self, tags ):
         super().__init__()
@@ -48,9 +86,11 @@ class myXmlContentHandler( QXmlDefaultHandler ):
             self.myTags.AddTag( localName )
         #print( "Read Start Tag : " + localName + "\n" )
         #print( "Tag Attributes: " )
-        #for num in range( 0, atts.length() ):    
-        #    self.controller.PrintToLog( atts.type( num ) + "=" + atts.value( num ) + "\n" )
-        #    self.controller.PrintToLog( "#####################################\n\n" )
+        #for num in range( 0, atts.length() ):
+        #    self.controller.PrintToLog( atts.type( num ) + "=" + atts.value(
+        #    num ) + "\n" )
+        #    self.controller.PrintToLog(
+        #    "#####################################\n\n" )
         return True
 
 
